cdb711a67c0dfceadc9d7a040efc78544c30a7e7
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-11-22 13:30:53 -0600

    Update install.sh

--- installer-scripts/install.sh
+++ installer-scripts/install.sh
@@ -2,4 +2,6 @@
 
 sudo useradd brew -d /home/.brewuser
 
+sudo rm -rf /home/linuxbrew
+
 sudo su brew -c "/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)""
